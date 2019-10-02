57fc5b6aede42c6261634d1339ca1d6f1e1d8cc8
Jo IE <joie.software@gmail.com>
2019-10-02 15:10:08 +0100

    test site name

--- gitConfig.php
+++ gitConfig.php
@@ -13,7 +13,7 @@ require_once 'Github_OAuth_Client.php';
  */
 $clientID         = '9f4b39eaad1c6ce235dc';
 $clientSecret     = '31e43e912cddcaf214a2861741ff7452231e29c2';
-$redirectURL     = 'https://kymobudget.herokuapp.com/github-login/';
+$redirectURL     = 'https://stormy-ridge-97319.herokuapp.com/github-login/';
 
 $gitClient = new Github_OAuth_Client(array(
     'client_id' => $clientID,
